151a4f165aced6bacd79959a1969c68e9cf81004
luis-tapia12 <j.contreras.t@hotmail.com>
2024-08-06 16:34:21 -0600

    add Input component

--- lib/main.ts
+++ lib/main.ts
@@ -5,5 +5,6 @@ export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Checkbox } from './Checkbox';
 export { default as Dropdown } from './Dropdown';
+export { default as Input } from './Input';
 export { default as Modal } from './Modal';
 export { default as Switch } from './Switch';
