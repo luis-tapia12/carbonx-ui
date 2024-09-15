8eff5ed13d77e1fc5f2b967b4446689b4ea24a0b
luis-tapia12 <j.contreras.t@hotmail.com>
2024-09-14 19:24:56 -0600

    add Badge component

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,7 @@
 import './styles.css';
 
 export { default as Avatar } from './Avatar';
+export { default as Badge } from './Badge';
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Checkbox } from './Checkbox';
